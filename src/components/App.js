03fa1b882f2fbbf165a751743a713a52d347f07f
Wimmind <terehov.anton@bk.ru>
2020-10-09 15:07:38 +0400

    add arrow

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import * as threeFunctions from './three';
 
-const img = 'https://i.imgur.com/GFLxXVV.jpg';
+import textures from '../data.json'
+
+//const img = 'https://i.imgur.com/GFLxXVV.jpg';
 
 export default class App extends Component {
+  state = {
+    num: 0
+  }
 
   componentDidMount() {
-   // threeFunctions.init(img)
-   // threeFunctions.animate();
+    const img = process.env.PUBLIC_URL + `/textures/${textures[0].src}`
+    threeFunctions.init(img)
+    threeFunctions.animate();
+  }
+
+  click = () => {
+   // let num = this.state.num + 20;
+    //const img = process.env.PUBLIC_URL + `/textures/${textures[1].src}`
+    threeFunctions.posCam()
+   // this.setState({num})
   }
 
   render() {
     return (
-      <div cl>
-
+      <div className='wrapper'>
+        <canvas id="canvas" width={800} height={500}></canvas>
+        <button onClick={this.click}>давай</button>
       </div>
     );
   }
